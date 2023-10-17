f9995447700317f38f32dc035b53a5165ab519c6
Alvaro Luis <alvaro500@outlook.com>
2023-10-17 11:23:32 -0500

    Interact whit LayerMask activo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,63 @@ public class Player : MonoBehaviour
 {
 	[SerializeField] private float _moveSpeed = 7f;
 	[SerializeField] private GameInput _gameInput;
+	[SerializeField] private LayerMask _countersLayerMask;
 	
 	private bool _isWalking;
+	private Vector3 _lastIteractDir;
 	
     private void Update()
     {
+		HandleMovement();
+		HandleOInteractions();
+    }
+	
+	public bool IsWalking()
+	{
+		return _isWalking;
+	}
+	
+	private void HandleOInteractions()
+	{
+		Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
+		
+		Vector3 moveDir = new Vector3 (inputVector.x, 0f, inputVector.y);
+		
+		if (moveDir != Vector3.zero)
+		{
+			_lastIteractDir = moveDir;
+		}
+		
+		float interactDistance = 2f;
+		//if(Physics.Raycast(transform.position, _lastIteractDir, out RaycastHit raycastHit, interactDistance))
+		if(Physics.Raycast(transform.position, _lastIteractDir, out RaycastHit raycastHit, interactDistance, _countersLayerMask))
+		{
+			//Debug.Log(raycastHit.transform);
+			
+			//Ambos if hacen exactamente lo mismo
+			if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+			{
+				  //Has ClearCounter 
+				  clearCounter.Interact();
+			}
+			
+			/*
+			ClearCounter clearCounter = raycastHit.transform.GetComponent<ClearCounter>();
+			if(clearCounter != null)
+			{
+				//Has ClearCounter 
+				
+			}
+			*/
+			
+		}else
+		{
+			Debug.Log("-");
+		}
+	}
+	
+	private void HandleMovement()
+	{
 		Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
 		
 		Vector3 moveDir = new Vector3 (inputVector.x, 0f, inputVector.y);
@@ -64,10 +116,5 @@ public class Player : MonoBehaviour
 		_isWalking = moveDir != Vector3.zero;
 		float rotationSpeed = 10f;
 		transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
-    }
-	
-	public bool IsWalking()
-	{
-		return _isWalking;
 	}
 }
